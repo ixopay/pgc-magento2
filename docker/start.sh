ff64a329ddaed4744f3389f71b870fff2d978a77
Daniel Weissengruber <d.weissengruber@ixolit.com>
2020-02-05 16:19:45 +0100

    fix permissions first

--- docker/start.sh
+++ docker/start.sh
@@ -60,12 +60,6 @@ if [ ! -f "/setup_complete" ]; then
         php -f /magento2-sample-data/dev/tools/build-sample-data.php -- --ce-source="/opt/bitnami/magento/htdocs/"
     fi
 
-    # Rebuild cache and classes
-    # php /opt/bitnami/magento/htdocs/bin/magento setup:upgrade
-    # chown -R bitnami:daemon /magento2-sample-data/pub/media/catalog
-    # php /opt/bitnami/magento/htdocs/bin/magento setup:di:compile
-    # php /opt/bitnami/magento/htdocs/bin/magento cache:flush
-
     echo -e "Configuring Magento"
 
     # Disable other payment Providers
@@ -121,24 +115,23 @@ if [ ! -f "/setup_complete" ]; then
         fix_symlink /opt/bitnami/magento/htdocs/app/etc /bitnami/magento/htdocs/app/etc
         fix_symlink /opt/bitnami/magento/htdocs/app/design /bitnami/magento/htdocs/app/design
         fix_symlink /opt/bitnami/magento/htdocs/app/code /bitnami/magento/htdocs/app/code
-        rm -rf /opt/bitnami/magento/htdocs/var/cache/* /opt/bitnami/magento/htdocs/var/page_cache/* /opt/bitnami/magento/htdocs/var/generation/* /opt/bitnami/magento/htdocs/app/design/code/Pgc
-        php /opt/bitnami/magento/htdocs/bin/magento setup:upgrade
-        php /opt/bitnami/magento/htdocs/bin/magento cache:flush
-        php /opt/bitnami/magento/htdocs/bin/magento setup:di:compile
         chown -R bitnami:daemon /opt/bitnami/magento/htdocs/ 
         chmod -R 775 /opt/bitnami/magento/htdocs/
         chmod -R 777 /opt/bitnami/magento/htdocs/generated/code/Magento/Config /opt/bitnami/magento/htdocs/pub/media/catalog/product /opt/bitnami/magento/htdocs/var
+        php /opt/bitnami/magento/htdocs/bin/magento setup:upgrade
+        php /opt/bitnami/magento/htdocs/bin/magento cache:flush
+        php /opt/bitnami/magento/htdocs/bin/magento setup:di:compile
 
         echo -e "Setup Complete! You can access the instance at: ${MAGENTO_HOST}"
 
         kill 1
     else
+        chown -R bitnami:daemon /opt/bitnami/magento/htdocs/
+        chmod -R 775 /opt/bitnami/magento/htdocs/
+        chmod -R 777 /opt/bitnami/magento/htdocs/generated/code/Magento/Config /opt/bitnami/magento/htdocs/pub/media/catalog/product /opt/bitnami/magento/htdocs/var
         php /opt/bitnami/magento/htdocs/bin/magento setup:upgrade
         php /opt/bitnami/magento/htdocs/bin/magento cache:flush
         php /opt/bitnami/magento/htdocs/bin/magento setup:di:compile
-        chown -R bitnami:daemon /opt/bitnami/magento/htdocs/ 
-        chmod -R 775 /opt/bitnami/magento/htdocs/
-        chmod -R 777 /opt/bitnami/magento/htdocs/generated/code/Magento/Config /opt/bitnami/magento/htdocs/pub/media/catalog/product /opt/bitnami/magento/htdocs/var
 
         echo -e "Setup Complete! You can access the instance at: ${MAGENTO_HOST}"
 
