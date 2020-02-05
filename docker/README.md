0943633832469c13249f41ed36d53949a92833c1
Daniel Weissengruber <d.weissengruber@ixolit.com>
2020-02-05 14:03:41 +0100

    Update env vars & readme, tested local deployments

--- docker/README.md
+++ docker/README.md
@@ -1,48 +1,104 @@
-**Warning!** This docker image is dedicated for demo usage, we don't recommended to use it in production.
+# Docker demo & development environment
+
+We supply ready to use Docker environments for plugin development & testing. 
+
+**Warning!** This docker image is dedicated for development & demo usage, we don't recommended to use it in production.
 
 ---
 
-# USAGE
+## Usage
 
 > **NOTE**: Elasticsearch dependency will not start successfully before you adjust `sysctl -w vm.max_map_count=262144` for your local Docker engine.
 > The following Stackoverflow article should explain steps for multiple Docker engines:
 > https://stackoverflow.com/questions/41192680/update-max-map-count-for-elasticsearch-docker-container-mac-host
 
-To quickly spawn a Magento test shop with a plugin tagged at our github.com repository:
+To quickly spawn a Magento test shop with a plugin tagged at our Github repository:
+Clone our plugin repository and run the following command from the plugin root directory:
 
-```
+```bash
+ # MAGENTO_PASSWORD must conatin a numeral consist of 8+ chars
  REPOSITORY="https://github.com/ixopay/pgc-magento2" \
  BRANCH="master" \
- MAGENTO_HOST="localhost" \
- MAGENTO_USERNAME=dev \
- MAGENTO_PASSWORD=dev \
+ MAGENTO_HOST="127.0.0.1" \
+ MAGENTO_USERNAME="dev" \
+ MAGENTO_PASSWORD="develop1" \
   docker-compose -f docker-compose.github.yml up --build --force-recreate --renew-anon-volumes
 ```
 
-To develop and test plugin changes, you can run the following docker-compose command from the plugin root directory, to start a Magento shop &
-initialize a database with a bind mounted version of the plugin. The shop will be accessible via: `http://localhost/admin`.
+To develop and test plugin changes, you can run the following docker-compose command from the plugin root directory, to start a Magento shop & initialize a database with a bind mounted version of the plugin. The shop will be accessible via: `http://127.0.0.1/admin`.
 
-```
- BITNAMI_IMAGE_VERSION=latest \
- MAGENTO_HOST="localhost" \
- MAGENTO_USERNAME=dev \
- MAGENTO_PASSWORD=dev \
+```bash
+ # MAGENTO_PASSWORD must conatin a numeral consist of 8+ chars
+ BITNAMI_IMAGE_VERSION="latest" \
+ MAGENTO_HOST="127.0.0.1" \
+ MAGENTO_USERNAME="dev" \
+ MAGENTO_PASSWORD="develop1" \
   docker-compose up --build --force-recreate --renew-anon-volumes
 ```
 
-By running the command we always run a complete `--build` for the shop container, `--force-recreate` to delete previous containers  and always delete
-the previous instance's storage volumes via `--renew-anon-volumes`. We currently use Bitnami Docker images as base for the environment and add our plugin.
-Further environment variables can be set, please take a look at `docker/Dockerfile` for a complete list.
+To test a build you generated via build.php run the following command from the plugin root directory:
 
-## Platform credentials
+```bash
+ # MAGENTO_PASSWORD must conatin a numeral consist of 8+ chars
+ php build.php sandbox.paymentgateway.cloud "My Payment Provider"
+ BITNAMI_IMAGE_VERSION="latest" \
+ BUILD_ARTIFACT="${PWD}/dist/magento2-my-payment-provider-1.0.0.zip" \
+ MAGENTO_HOST="127.0.0.1" \
+ MAGENTO_USERNAME="dev" \
+ MAGENTO_PASSWORD="develop1" \
+  docker-compose up --build --force-recreate --renew-anon-volumes
+```
+
+Please note:
+
+- By running the command we always run a complete `--build` for the shop container, `--force-recreate` to delete previous containers and always delete the previous instance's storage volumes via `--renew-anon-volumes`. We don't support to change variables without rebuilding the full container.
+- We currently use Bitnami Docker images as base for the environment and add our plugin.
+- Further environment variables can be set, please take a look at `docker/Dockerfile` for a complete list.
+
+### Customize Settings
+
+Defaults for the Docker build are configured in the `docker-compose` files. You can either:
+ - set variables via environment variable or (like above)
+ - persist them in the `environment:` section of the respective docker-compose file.
+
+### Platform credentials
 
 To successfully test a payment flow you will need merchant credentials for the payment platform and set them via the following environment variables:
 
-```
+> These Options are ignored when using an pre-generated zip-file!
+> Please Configure the Payment-Settings via the Admin-Interface (e.g.: https://127.0.0.1/admin)
+
+```bash
+ # Base url for payment plaform API
  SHOP_PGC_URL="https://sandbox.paymentgateway.cloud"
+ # Credentials for payment platform API
  SHOP_PGC_USER="test-user"
  SHOP_PGC_PASSWORD="test-pass"
  SHOP_PGC_API_KEY="key"
  SHOP_PGC_SECRET="secret"
- SHOP_PGC_INTEGRATION_KEY="intkey"
+ SHOP_PGC_INTEGRATION_KEY="int-key"
 ```
+
+Additional platform specific settings:
+
+```bash
+ # Enable or disable payments for specific schemes
+ SHOP_PGC_CC_AMEX="True"
+ SHOP_PGC_CC_DINERS="True"
+ SHOP_PGC_CC_DISCOVER="True"
+ SHOP_PGC_CC_JCB="True"
+ SHOP_PGC_CC_MAESTRO="True"
+ SHOP_PGC_CC_MASTERCARD="True"
+ SHOP_PGC_CC_UNIOPNPAY="True"
+ SHOP_PGC_CC_VISA="True"
+ # Either use "debit" or "preauthorize" transaction requests
+ SHOP_PGC_CC_TYPE="debit"
+ SHOP_PGC_CC_TYPE_AMEX="debit"
+ SHOP_PGC_CC_TYPE_DINERS="debit"
+ SHOP_PGC_CC_TYPE_DISCOVER="debit"
+ SHOP_PGC_CC_TYPE_JCB="debit"
+ SHOP_PGC_CC_TYPE_MAESTRO="debit"
+ SHOP_PGC_CC_TYPE_MASTERCARD="debit"
+ SHOP_PGC_CC_TYPE_UNIOPNPAY="debit"
+ SHOP_PGC_CC_TYPE_VISA="debit"
+ ```
--- docker/start.sh
+++ docker/start.sh
@@ -20,31 +20,49 @@ if [ ! -f "/setup_complete" ]; then
 
     while (! $(curl --silent -H "Host: ${MAGENTO_HOST}" http://localhost:80/index.php/ | grep "CMS homepage" > /dev/null)); do sleep 2s; done
 
-    php /opt/bitnami/magento/htdocs/bin/magento config:set web/secure/use_in_frontend 0
-    if [ $PRECONFIGURE ]; then
-        php /opt/bitnami/magento/htdocs/bin/magento config:set web/secure/use_in_adminhtml 1
-    else
-        php /opt/bitnami/magento/htdocs/bin/magento config:set web/secure/use_in_adminhtml 0
-    fi
-
     echo -e "Installing PGC Extension"
 
-    if [ ! -d "/source/.git" ] && [ ! -f  "/source/.git" ]; then
-        echo -e "Checking out branch ${BRANCH} from ${REPOSITORY}"
-        git clone $REPOSITORY /tmp/paymentgatewaycloud
-        cd /tmp/paymentgatewaycloud
-        git checkout $BRANCH
-        mkdir -p /opt/bitnami/magento/htdocs/app/code
-        cp -R /tmp/paymentgatewaycloud/* /opt/bitnami/magento/htdocs/app/code/
+    mkdir -p /opt/bitnami/magento/htdocs/app/code/
+    if [ "${BUILD_ARTIFACT}" != "undefined" ]; then
+        if [ -f /dist/paymentgatewaycloud.zip ]; then
+            echo -e "Using Supplied zip ${BUILD_ARTIFACT}"
+            ZIP_NAME=$(basename "${BUILD_ARTIFACT}")
+            mkdir /tmp/source
+            unzip /dist/paymentgatewaycloud.zip -d /tmp/source
+            cp -R /tmp/source/* /opt/bitnami/magento/htdocs/app/code/
+        else
+            echo "Faled to build!, there is no such file: ${BUILD_ARTIFACT}"
+            exit 1
+        fi
     else
-        echo -e "Using Development Source!"
-        mkdir -p /opt/bitnami/magento/htdocs/app/code/
-        cp -R /source/* /opt/bitnami/magento/htdocs/app/code/
+        if [ ! -d "/source/.git" ] && [ ! -f  "/source/.git" ]; then
+            echo -e "Checking out branch ${BRANCH} from ${REPOSITORY}"
+            git clone $REPOSITORY /tmp/paymentgatewaycloud
+            cd /tmp/paymentgatewaycloud
+            git checkout $BRANCH
+            cp -R /tmp/paymentgatewaycloud/* /opt/bitnami/magento/htdocs/app/code/
+        else
+            echo -e "Using Development Source!"
+            cp -R /source/* /opt/bitnami/magento/htdocs/app/code/
+        fi
     fi
     chown -R bitnami:daemon /opt/bitnami/magento/htdocs
 
     php /opt/bitnami/magento/htdocs/bin/magento module:enable Pgc_Pgc
+
+    echo -e "Import Products"
+
+    if [ ! -d "/magento2-sample-data" ]; then
+        echo -e "Checking out branch 2.3.3 from https://github.com/magento/magento2-sample-data"
+        git clone https://github.com/magento/magento2-sample-data /magento2-sample-data
+        cd /magento2-sample-data
+        git checkout 2.3.3
+        php -f /magento2-sample-data/dev/tools/build-sample-data.php -- --ce-source="/opt/bitnami/magento/htdocs/"
+    fi
+
+    # Rebuild cache and classes
     php /opt/bitnami/magento/htdocs/bin/magento setup:upgrade
+    chown -R bitnami:daemon /magento2-sample-data/pub/media/catalog
     php /opt/bitnami/magento/htdocs/bin/magento setup:di:compile
     php /opt/bitnami/magento/htdocs/bin/magento cache:flush
 
@@ -81,18 +99,17 @@ if [ ! -f "/setup_complete" ]; then
         php /opt/bitnami/magento/htdocs/bin/magento config:set payment/pgc_creditcard/sort_order 1
     fi
 
-    echo -e "Import Products"
-
-    if [ ! -d "/magento2-sample-data" ]; then
-        echo -e "Checking out branch 2.3.3 from https://github.com/magento/magento2-sample-data"
-        git clone https://github.com/magento/magento2-sample-data /magento2-sample-data
-        cd /magento2-sample-data
-        git checkout 2.3.3
-        php -f /magento2-sample-data/dev/tools/build-sample-data.php -- --ce-source="/opt/bitnami/magento/htdocs/"
-        php /opt/bitnami/magento/htdocs/bin/magento setup:upgrade
-        chown -R bitnami:daemon /magento2-sample-data/pub/media/catalog
+    # Where to use https per default
+    php /opt/bitnami/magento/htdocs/bin/magento config:set web/secure/use_in_frontend 0
+    if [ $PRECONFIGURE ]; then
+        php /opt/bitnami/magento/htdocs/bin/magento config:set web/unsecure/base_url "http://${MAGENTO_HOST}/"
+        php /opt/bitnami/magento/htdocs/bin/magento config:set web/secure/base_url "https://${MAGENTO_HOST}/"
+        php /opt/bitnami/magento/htdocs/bin/magento config:set web/secure/use_in_adminhtml 1
+    else
+        php /opt/bitnami/magento/htdocs/bin/magento config:set web/unsecure/base_url "http://${MAGENTO_HOST}/"
+        php /opt/bitnami/magento/htdocs/bin/magento config:set web/secure/base_url "http://${MAGENTO_HOST}/"
+        php /opt/bitnami/magento/htdocs/bin/magento config:set web/secure/use_in_adminhtml 0
     fi
-
 
     touch /setup_complete
 
@@ -105,23 +122,23 @@ if [ ! -f "/setup_complete" ]; then
         fix_symlink /opt/bitnami/magento/htdocs/app/design /bitnami/magento/htdocs/app/design
         fix_symlink /opt/bitnami/magento/htdocs/app/code /bitnami/magento/htdocs/app/code
         rm -rf /opt/bitnami/magento/htdocs/var/cache/* /opt/bitnami/magento/htdocs/var/page_cache/* /opt/bitnami/magento/htdocs/var/generation/* /opt/bitnami/magento/htdocs/app/design/code/Pgc
-        php /opt/bitnami/magento/htdocs/bin/magento setup:di:compile
         php /opt/bitnami/magento/htdocs/bin/magento cache:flush
+        php /opt/bitnami/magento/htdocs/bin/magento setup:di:compile
         chown -R bitnami:daemon /opt/bitnami/magento/htdocs/ 
         chmod -R 775 /opt/bitnami/magento/htdocs/
         chmod -R 777 /opt/bitnami/magento/htdocs/generated/code/Magento/Config /opt/bitnami/magento/htdocs/pub/media/catalog/product /opt/bitnami/magento/htdocs/var
 
-        echo -e "Setup Complete"
+        echo -e "Setup Complete! You can access the instance at: ${MAGENTO_HOST}"
 
         kill 1
     else
-        # Update URLs 
-        php /opt/bitnami/magento/htdocs/bin/magento config:set web/unsecure/base_url "http://${MAGENTO_HOST}/"
-        php /opt/bitnami/magento/htdocs/bin/magento config:set web/secure/base_url "https://${MAGENTO_HOST}/"
-        php /opt/bitnami/magento/htdocs/bin/magento setup:di:compile
         php /opt/bitnami/magento/htdocs/bin/magento cache:flush
+        php /opt/bitnami/magento/htdocs/bin/magento setup:di:compile
+        chown -R bitnami:daemon /opt/bitnami/magento/htdocs/ 
+        chmod -R 775 /opt/bitnami/magento/htdocs/
+        chmod -R 777 /opt/bitnami/magento/htdocs/generated/code/Magento/Config /opt/bitnami/magento/htdocs/pub/media/catalog/product /opt/bitnami/magento/htdocs/var
 
-        echo -e "Setup Complete"
+        echo -e "Setup Complete! You can access the instance at: ${MAGENTO_HOST}"
 
         # Keep script Running
         trap : TERM INT; (while true; do sleep 1m; done) & wait
@@ -130,9 +147,9 @@ if [ ! -f "/setup_complete" ]; then
 else
 
     # Update URL
-    sleep 30s
-    php /opt/bitnami/magento/htdocs/bin/magento config:set web/unsecure/base_url "http://${MAGENTO_HOST}/"
-    php /opt/bitnami/magento/htdocs/bin/magento config:set web/secure/base_url "https://${MAGENTO_HOST}/"
+    # sleep 30s
+    # php /opt/bitnami/magento/htdocs/bin/magento config:set web/unsecure/base_url "http://${MAGENTO_HOST}/"
+    # php /opt/bitnami/magento/htdocs/bin/magento config:set web/secure/base_url "https://${MAGENTO_HOST}/"
 
 
     # Keep script Running
