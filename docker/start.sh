5eac6291eba98721fee6dcf5e9cf843ea6dd187e
Daniel Weissengruber <d.weissengruber@ixolit.com>
2020-02-13 13:10:22 +0100

    re-arrange stuff

--- docker/start.sh
+++ docker/start.sh
@@ -60,6 +60,12 @@ if [ ! -f "/setup_complete" ]; then
         php -f /magento2-sample-data/dev/tools/build-sample-data.php -- --ce-source="/opt/bitnami/magento/htdocs/"
     fi
 
+    # Rebuild cache and classes
+    # php /opt/bitnami/magento/htdocs/bin/magento setup:upgrade
+    # chown -R bitnami:daemon /magento2-sample-data/pub/media/catalog
+    # php /opt/bitnami/magento/htdocs/bin/magento setup:di:compile
+    # php /opt/bitnami/magento/htdocs/bin/magento cache:flush
+
     echo -e "Configuring Magento"
 
     # Disable other payment Providers
@@ -109,38 +115,31 @@ if [ ! -f "/setup_complete" ]; then
 
     if [ $PRECONFIGURE ]; then
         echo -e "Prepare for Pre-Configured build"
-        php /opt/bitnami/magento/htdocs/bin/magento setup:upgrade
-        php /opt/bitnami/magento/htdocs/bin/magento setup:di:compile
-
-        # Move data away from volume (would get lost after build)
         fix_symlink /opt/bitnami/magento/htdocs/var /bitnami/magento/htdocs/var
         fix_symlink /opt/bitnami/magento/htdocs/pub/static /bitnami/magento/htdocs/pub/static
         fix_symlink /opt/bitnami/magento/htdocs/pub/media /bitnami/magento/htdocs/pub/media
         fix_symlink /opt/bitnami/magento/htdocs/app/etc /bitnami/magento/htdocs/app/etc
         fix_symlink /opt/bitnami/magento/htdocs/app/design /bitnami/magento/htdocs/app/design
         fix_symlink /opt/bitnami/magento/htdocs/app/code /bitnami/magento/htdocs/app/code
-        
-        # Fix Permissions
-        chown -R bitnami:daemon /opt/bitnami/magento/htdocs/
-        chown -R bitnami:daemon /magento2-sample-data/pub
-        chmod -R 775 /opt/bitnami/magento/htdocs/
-        mkdir /opt/bitnami/magento/htdocs/pub/media/catalog/product
-        chmod -R 777 /opt/bitnami/magento/htdocs/generated/code/Magento/Config /opt/bitnami/magento/htdocs/pub/media/catalog/product /opt/bitnami/magento/htdocs/var
-        
-        # Clear cache
+
         rm -rf /opt/bitnami/magento/htdocs/var/cache/* /opt/bitnami/magento/htdocs/var/page_cache/* /opt/bitnami/magento/htdocs/var/generation/* /opt/bitnami/magento/htdocs/app/design/code/Pgc
         php /opt/bitnami/magento/htdocs/bin/magento cache:flush
+
+        php /opt/bitnami/magento/htdocs/bin/magento setup:di:compile
+        
+        chown -R bitnami:daemon /opt/bitnami/magento/htdocs/ 
+        chmod -R 775 /opt/bitnami/magento/htdocs/
+        chmod -R 777 /opt/bitnami/magento/htdocs/generated/code/Magento/Config /opt/bitnami/magento/htdocs/pub/media/catalog/product /opt/bitnami/magento/htdocs/var
 
         echo -e "Setup Complete! You can access the instance at: ${MAGENTO_HOST}"
 
         kill 1
     else
-        chown -R bitnami:daemon /opt/bitnami/magento/htdocs/
+        php /opt/bitnami/magento/htdocs/bin/magento cache:flush
+        php /opt/bitnami/magento/htdocs/bin/magento setup:di:compile
+        chown -R bitnami:daemon /opt/bitnami/magento/htdocs/ 
         chmod -R 775 /opt/bitnami/magento/htdocs/
         chmod -R 777 /opt/bitnami/magento/htdocs/generated/code/Magento/Config /opt/bitnami/magento/htdocs/pub/media/catalog/product /opt/bitnami/magento/htdocs/var
-        php /opt/bitnami/magento/htdocs/bin/magento setup:upgrade
-        php /opt/bitnami/magento/htdocs/bin/magento setup:di:compile
-        php /opt/bitnami/magento/htdocs/bin/magento cache:flush
 
         echo -e "Setup Complete! You can access the instance at: ${MAGENTO_HOST}"
 
@@ -150,15 +149,11 @@ if [ ! -f "/setup_complete" ]; then
 
 else
 
-    # Fix Permissions
-    # chown -R bitnami:daemon /opt/bitnami/magento/htdocs/
-    # chown -R bitnami:daemon /magento2-sample-data/pub
-    # chmod -R 775 /opt/bitnami/magento/htdocs/
-    # # php /opt/bitnami/magento/htdocs/bin/magento setup:upgrade
-    # # php /opt/bitnami/magento/htdocs/bin/magento setup:di:compile
-    # php /opt/bitnami/magento/htdocs/bin/magento cache:flush
-    # mkdir /opt/bitnami/magento/htdocs/pub/media/catalog/product
-    # chmod -R 777 /opt/bitnami/magento/htdocs/generated/code/Magento/Config /opt/bitnami/magento/htdocs/pub/media/catalog/product /opt/bitnami/magento/htdocs/var
+    # Update URL
+    # sleep 30s
+    # php /opt/bitnami/magento/htdocs/bin/magento config:set web/unsecure/base_url "http://${MAGENTO_HOST}/"
+    # php /opt/bitnami/magento/htdocs/bin/magento config:set web/secure/base_url "https://${MAGENTO_HOST}/"
+
 
     # Keep script Running
     trap : TERM INT; (while true; do sleep 1m; done) & wait
